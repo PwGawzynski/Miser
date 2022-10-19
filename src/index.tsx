f95ac382d55aba36addfdbb92d05e308df0767d9
grzesikb <grzesikb@wp.pl>
2022-10-19 21:57:35 +0200

    Test - First use - Bartek

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ export default function App(): JSX.Element {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-bubble-gum">KURWAAAA DZIAŁAJ </Text>
+      <Text className="text-bubble-gum">TEST KUPA 123</Text>
       <View className="bg-midnight"></View>
       <StatusBar style="auto" />
     </View>
