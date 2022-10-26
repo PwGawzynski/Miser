ee6870d110bb58f8fbe65787bf40b9d7b1177523
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-26 23:44:28 +0200

    created sample component for testing router

--- src/Components/Pages/AccountSettings.tsx
+++ src/Components/Pages/AccountSettings.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../index';
+import { SafeAreaView } from 'react-native-safe-area-context';
 type Props = NativeStackScreenProps<RootStackParamList, 'AccountSettings'>;
 export const AccountSettings = ({ navigation, route }: Props) => {
   return (
-    <View>
-      <Text>Witam w innym domu kurwa.</Text>
-      <Button
-        title="Go to The Home"
-        onPress={() => navigation.navigate('Home')}
-      />
-    </View>
+    <SafeAreaView className={'min-h-screen bg-midnight'}>
+      <View className={'min-w-max flex-1 items-center justify-center bg-metal'}>
+        <Text className={'text-white'}>Witam w Account Filters kurwa.</Text>
+      </View>
+    </SafeAreaView>
   );
 };
