dc09d06eb83ed5dbba18c10bc477a7f2cb1bef82
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-27 01:19:02 +0200

    added UserCard

--- src/Navigation/Drawer/CustomDrawer.tsx
+++ src/Navigation/Drawer/CustomDrawer.tsx
@@ -9,11 +9,14 @@ import Home from '../../assets/Home.svg';
 import AccountSettingsSVG from '../../assets/AccountSettings.svg';
 import FiltersSVG from '../../assets/Filters.svg';
 import AchievementsSVG from '../../assets/Achievements.svg';
+import LogOutSVG from '../../assets/Logout.svg';
+import { UserAccountCard } from '../../Components/Organisms/UserAccountCard';
 
 export function CustomDrawer(props: DrawerContentComponentProps) {
   return (
     <DrawerContentScrollView {...props}>
       <View className={'items-center justify-center bg-black pl-11'}>
+        <UserAccountCard />
         {/* home drawer menu BTN */}
         <DrawerMenuItem {...props} name={'Home'} nameForNavigator={'Home'}>
           <Home />
@@ -44,7 +47,7 @@ export function CustomDrawer(props: DrawerContentComponentProps) {
           name={'Log Out'}
           nameForNavigator={'Achievements'}
         >
-          <AchievementsSVG />
+          <LogOutSVG />
         </DrawerMenuItem>
       </View>
     </DrawerContentScrollView>
