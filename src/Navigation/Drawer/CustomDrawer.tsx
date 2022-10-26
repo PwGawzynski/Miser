1700d678c54ce4832483c2610d1338d08bb4927e
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-26 23:45:51 +0200

    done all btns for custom drawer, added possibility to redirect on click to given screen

--- src/Navigation/Drawer/CustomDrawer.tsx
+++ src/Navigation/Drawer/CustomDrawer.tsx
@@ -5,18 +5,47 @@ import {
 } from '@react-navigation/drawer';
 import { View } from 'react-native';
 import { DrawerMenuItem } from '../../Components/Organisms/DrawerMenuItem';
+import Home from '../../assets/Home.svg';
+import AccountSettingsSVG from '../../assets/AccountSettings.svg';
+import FiltersSVG from '../../assets/Filters.svg';
+import AchievementsSVG from '../../assets/Achievements.svg';
 
-export interface CustomDrawerProps extends DrawerContentComponentProps {
-  screenList: string[];
-}
-
-export function CustomDrawer(props: CustomDrawerProps) {
+export function CustomDrawer(props: DrawerContentComponentProps) {
   return (
     <DrawerContentScrollView {...props}>
-      <View className={'items-center justify-center bg-metal'}>
-        {props.screenList.map((screenName) => (
-          <DrawerMenuItem screenName={screenName} key={screenName} />
-        ))}
+      <View className={'items-center justify-center bg-black pl-11'}>
+        {/* home drawer menu BTN */}
+        <DrawerMenuItem {...props} name={'Home'} nameForNavigator={'Home'}>
+          <Home />
+        </DrawerMenuItem>
+        <DrawerMenuItem
+          {...props}
+          name={'Account Settings'}
+          nameForNavigator={'AccountSettings'}
+        >
+          <AccountSettingsSVG />
+        </DrawerMenuItem>
+        <DrawerMenuItem
+          {...props}
+          name={'Filters'}
+          nameForNavigator={'Filters'}
+        >
+          <FiltersSVG />
+        </DrawerMenuItem>
+        <DrawerMenuItem
+          {...props}
+          name={'Achievements'}
+          nameForNavigator={'Achievements'}
+        >
+          <AchievementsSVG />
+        </DrawerMenuItem>
+        <DrawerMenuItem
+          {...props}
+          name={'Log Out'}
+          nameForNavigator={'Achievements'}
+        >
+          <AchievementsSVG />
+        </DrawerMenuItem>
       </View>
     </DrawerContentScrollView>
   );
