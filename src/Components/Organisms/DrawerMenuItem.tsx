cdf8923a8fcb381af1c5f0e945b2ad5d562f769b
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-28 10:53:44 +0200

    Changed to touchable

--- src/Components/Organisms/DrawerMenuItem.tsx
+++ src/Components/Organisms/DrawerMenuItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
 import { LargeLabel } from '../Atoms/LargeLabel';
 
