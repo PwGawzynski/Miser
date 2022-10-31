867bc4ea4cd1866e35284f0d309fc41bb663f968
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-31 08:43:50 +0100

    Created login page

--- src/Components/Pages/LoginPage.tsx
+++ src/Components/Pages/LoginPage.tsx
@@ -1,22 +1,48 @@
-import React, { useContext } from 'react';
-import { Button } from 'react-native';
+import React, { useContext, useState } from 'react';
 import { AuthContextProvider } from '../../index';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { LoginForm } from '../Molecules/LoginForm';
+import { AppButton } from '../Atoms/AppButton';
+import { SmallNote } from '../Molecules/SmallNote';
+import { PressableText } from '../Atoms/PressableText';
 
 export const LoginPage = () => {
   const setter = useContext(AuthContextProvider);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <SafeAreaView className={'flex-column flex-1 bg-orange'}>
-      <Button
-        color={'white'}
-        title={'Login'}
+    <SafeAreaView
+      className={
+        'flex-column w-screen flex-1 items-center justify-center bg-black'
+      }
+    >
+      <LoginForm
+        login={login}
+        setLogin={setLogin}
+        password={password}
+        setPassword={setPassword}
+      />
+      <AppButton
         onPress={() =>
           setter?.login({
-            login: 'user',
-            password: '123',
+            login,
+            password,
           })
         }
+        textContext={'LOGIN'}
       />
+      <SmallNote
+        childrenContentOn={true}
+        firstPartText={"You don't have an account?"}
+        firstPartTextCustomStyles={' ml-0'}
+      >
+        <PressableText
+          onPress={() => console.log('GoTO register')}
+          customTextContentStyles={' ml-2'}
+        >
+          CREATE NOW
+        </PressableText>
+      </SmallNote>
     </SafeAreaView>
   );
 };
