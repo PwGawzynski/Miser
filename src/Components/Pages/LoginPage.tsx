b5db0bec29573cf6c107d493be7a4f6f7dbc21f2
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-01 08:17:57 +0100

    added logo and navigation

--- src/Components/Pages/LoginPage.tsx
+++ src/Components/Pages/LoginPage.tsx
@@ -1,21 +1,29 @@
 import React, { useContext, useState } from 'react';
-import { AuthContextProvider } from '../../index';
+import { AuthContextProvider, AuthRootStackParamList } from '../../index';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LoginForm } from '../Molecules/LoginForm';
 import { AppButton } from '../Atoms/AppButton';
 import { SmallNote } from '../Molecules/SmallNote';
 import { PressableText } from '../Atoms/PressableText';
+import { Logo } from '../Atoms/Logo';
+import { Text } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+type Props = NativeStackScreenProps<AuthRootStackParamList, 'LoginPage'>;
 
-export const LoginPage = () => {
+export const LoginPage = ({ navigation }: Props) => {
   const setter = useContext(AuthContextProvider);
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+
   return (
     <SafeAreaView
       className={
         'flex-column w-screen flex-1 items-center justify-center bg-black'
       }
     >
+      <Logo>
+        <Text className={'text-xl text-white'}>LOG IN</Text>
+      </Logo>
       <LoginForm
         login={login}
         setLogin={setLogin}
@@ -29,7 +37,7 @@ export const LoginPage = () => {
             password,
           })
         }
-        textContext={'LOGIN'}
+        textContext={'LOG IN'}
       />
       <SmallNote
         childrenContentOn={true}
@@ -37,7 +45,7 @@ export const LoginPage = () => {
         firstPartTextCustomStyles={' ml-0'}
       >
         <PressableText
-          onPress={() => console.log('GoTO register')}
+          onPress={() => navigation.navigate('RegisterPage')}
           customTextContentStyles={' ml-2'}
         >
           CREATE NOW
