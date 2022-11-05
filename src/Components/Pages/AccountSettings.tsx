f1d47eb6c90364bd05297f51e4c46ba1104bbb02
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-05 11:24:16 +0100

    Added top bar

--- src/Components/Pages/AccountSettings.tsx
+++ src/Components/Pages/AccountSettings.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../index';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { RootStackParamList } from '../Organisms/DrawerMenu';
+import { TopBar } from '../Organisms/TopBar';
 type Props = NativeStackScreenProps<RootStackParamList, 'AccountSettings'>;
 export const AccountSettings = ({ navigation, route }: Props) => {
   return (
     <SafeAreaView className={'min-h-screen bg-midnight'}>
-      <View className={'min-w-max flex-1 items-center justify-center bg-metal'}>
-        <Text className={'text-white'}>Witam w Account Filters kurwa.</Text>
-      </View>
+      <TopBar navigation={navigation} route={route} />
+      <View
+        className={'min-w-max flex-1 items-center justify-center bg-metal'}
+      ></View>
     </SafeAreaView>
   );
 };
