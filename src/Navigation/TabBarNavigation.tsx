48dbb5ef8019eff3eb85e000fffa2c34370c624d
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-07 07:29:00 +0100

    added types

--- src/Navigation/TabBarNavigation.tsx
+++ src/Navigation/TabBarNavigation.tsx
@@ -1,55 +1,61 @@
 import React from 'react';
-import {Text, View, Button, Animated, StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { Good } from '../Components/Pages/Good';
 import { TheBest } from '../Components/Pages/TheBest';
 import { New } from '../Components/Pages/New';
-import { setStatusBarBackgroundColor } from 'expo-status-bar';
 
-const Tab = createMaterialTopTabNavigator();
+export type TabNavigatorParamList = {
+  New: undefined;
+  Good: undefined;
+  TheBest: undefined;
+};
+
+const Tab = createMaterialTopTabNavigator<TabNavigatorParamList>();
 
 function MyTabs() {
-    return(
-        <Tab.Navigator
-            initialRouteName='TheBest'
-            screenOptions={{
-                tabBarLabelStyle: {fontSize: 20, fontWeight: 'bold', color: '#fff'},
-                tabBarItemStyle: {width: 120},
-                tabBarStyle: {backgroundColor: 'black'},
-                tabBarIndicatorStyle: {backgroundColor: '#fff', width: 40, left: 40, bottom: 10, height: 4}
-            }}
-            >
-                <Tab.Screen 
-                    name="TheBest"
-                    component={TheBest}
-                    options={{tabBarLabel: 'The Best'}}
-                />
-                <Tab.Screen
-                    name="Good"
-                    component={Good}
-                    options={{tabBarLabel: 'Good'}}
-                />
-                <Tab.Screen
-                    name="New"
-                    component={New}
-                    options={{tabBarLabel: 'New'}}
-                />
-
-        </Tab.Navigator>
-    )
+  return (
+    <Tab.Navigator
+      initialRouteName="TheBest"
+      screenOptions={{
+        tabBarLabelStyle: { fontSize: 20, fontWeight: 'bold', color: '#fff' },
+        tabBarItemStyle: { width: 120 },
+        tabBarStyle: { backgroundColor: 'black' },
+        tabBarIndicatorStyle: {
+          backgroundColor: '#fff',
+          width: 40,
+          left: 40,
+          bottom: 10,
+          height: 4,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="TheBest"
+        component={TheBest}
+        options={{ tabBarLabel: 'The Best' }}
+      />
+      <Tab.Screen
+        name="Good"
+        component={Good}
+        options={{ tabBarLabel: 'Good' }}
+      />
+      <Tab.Screen name="New" component={New} options={{ tabBarLabel: 'New' }} />
+    </Tab.Navigator>
+  );
 }
 
 const styles = StyleSheet.create({
-topNavi: {
+  topNavi: {
     backgroundColor: '#000',
-    },
-}) 
+  },
+});
 
 export default function TopBarNavigator() {
-    return (
-        <NavigationContainer independent={true}>
-            <MyTabs></MyTabs>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer independent={true}>
+      <MyTabs></MyTabs>
+    </NavigationContainer>
+  );
 }
