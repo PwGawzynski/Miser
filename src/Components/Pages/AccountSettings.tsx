5aecaace18da26a0c3b446af3ded83eef0d19087
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-07 23:17:00 +0100

    created account settings component with connection to firebase and post new login

--- src/Components/Pages/AccountSettings.tsx
+++ src/Components/Pages/AccountSettings.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -6,9 +6,18 @@ import { RootStackParamList } from '../Organisms/DrawerMenu';
 import { ScreenNameWBackArrow } from '../Molecules/ScreenNameWBackArrow';
 import { UserProfilePhoto } from '../Organisms/UserProfilePhoto';
 import { AppButton } from '../Atoms/AppButton';
+import { UserContext } from '../../index';
+import { CustomInput } from '../Atoms/CustomInput';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { db } from '../../../config';
+import { SmallNote } from '../Molecules/SmallNote';
+import { PressableText } from '../Atoms/PressableText';
 type Props = NativeStackScreenProps<RootStackParamList, 'AccountSettings'>;
 export const AccountSettings = ({ navigation, route }: Props) => {
-  console.log(navigation);
+  const userDataProvider = useContext(UserContext);
+
+  const [userName, setUserName] = useState('');
+
   return (
     <SafeAreaView className={'min-h-screen bg-black'}>
       <ScreenNameWBackArrow
@@ -25,6 +34,45 @@ export const AccountSettings = ({ navigation, route }: Props) => {
           }}
           textContext={'CHANGE IMAGE'}
         />
+        <CustomInput
+          value={userName}
+          setter={setUserName}
+          defaultValue={userDataProvider?.userData.login ?? ''}
+          customStyles={'pt-32'}
+        />
+        <AppButton
+          onPress={() => {
+            userDataProvider?.setUserData({
+              login: userName,
+              password: userDataProvider?.userData.password,
+            });
+            void (async () => {
+              const userRef = doc(db, 'user', 'GGE7ch9o6f3wxPf7JBd2');
+              await setDoc(userRef, { login: userName }, { merge: true });
+            })();
+          }}
+          textContext={'SAVE'}
+        />
+        <SmallNote firstPartText={'Reset All data'} childrenContentOn={true}>
+          <PressableText
+            onPress={() => {
+              void (async () => {
+                const userRef = doc(db, 'user', 'GGE7ch9o6f3wxPf7JBd2');
+                const defUser = doc(db, 'user', 'pL6F37UkxvL8Rwdj7KIc');
+                const docSNap = await getDoc(defUser);
+                if (!docSNap.exists()) return;
+                const defLogin = (docSNap.data()?.login as string) ?? '';
+                await setDoc(userRef, { login: defLogin }, { merge: true });
+                userDataProvider?.setUserData({
+                  login: defLogin,
+                  password: userDataProvider?.userData.password,
+                });
+              })();
+            }}
+          >
+            RESET
+          </PressableText>
+        </SmallNote>
       </View>
     </SafeAreaView>
   );
