6822fe6f0d85b08bcf7bf8e2014fe7b0def067ca
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-07 08:20:24 +0100

    Added components

--- src/Components/Pages/AccountSettings.tsx
+++ src/Components/Pages/AccountSettings.tsx
@@ -3,15 +3,29 @@ import { View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RootStackParamList } from '../Organisms/DrawerMenu';
-import { TopBar } from '../Organisms/TopBar';
+import { ScreenNameWBackArrow } from '../Molecules/ScreenNameWBackArrow';
+import { UserProfilePhoto } from '../Organisms/UserProfilePhoto';
+import { AppButton } from '../Atoms/AppButton';
 type Props = NativeStackScreenProps<RootStackParamList, 'AccountSettings'>;
 export const AccountSettings = ({ navigation, route }: Props) => {
+  console.log(navigation);
   return (
-    <SafeAreaView className={'min-h-screen bg-midnight'}>
-      <TopBar navigation={navigation} route={route} />
+    <SafeAreaView className={'min-h-screen bg-black'}>
+      <ScreenNameWBackArrow
+        screenName={'Account settings'}
+        onPress={() => navigation.goBack()}
+      ></ScreenNameWBackArrow>
       <View
-        className={'min-w-max flex-1 items-center justify-center bg-metal'}
-      ></View>
+        className={'min-w-max flex-1 items-center justify-start bg-black pt-20'}
+      >
+        <UserProfilePhoto />
+        <AppButton
+          onPress={() => {
+            console.log('CHANGE IMAGE CLICKED');
+          }}
+          textContext={'CHANGE IMAGE'}
+        />
+      </View>
     </SafeAreaView>
   );
 };
