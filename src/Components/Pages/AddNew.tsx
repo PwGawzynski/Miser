a0969098ac23592f592785e2537f42ece72d1936
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-12 20:10:04 +0100

    Created add new screen

---
+++ src/Components/Pages/AddNew.tsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from "react";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../Organisms/DrawerMenu';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScreenNameWBackArrow } from '../Molecules/ScreenNameWBackArrow';
+import { NewDealForm } from "../Organisms/NewDealForm";
+
+
+type Props = NativeStackScreenProps<RootStackParamList, 'AddNew'>;
+
+export interface DealInfoObj {
+  link: string;
+  name: string;
+  companyName: string;
+  desc: string;
+  price: string;
+  oldPrice: string;
+  shoppingCost: string;
+  dateOf: string;
+}
+
+
+export const AddNew = ({ navigation, route }: Props) => {
+
+
+  // screen name can be removed, to do this provide empty string to
+  // screenName prop
+  return (
+    <SafeAreaView
+      className={'h-screen w-screen items-center justify-start bg-black p-0'}
+    >
+      <ScreenNameWBackArrow
+        screenName={'ADD NEW'}
+        onPress={navigation.goBack}
+      />
+      <NewDealForm/>
+    </SafeAreaView>
+  );
+};
