1df5093a55af67615300106b80c36ea80aca9261
pwgawzynski <pgawzynski.backup@gmail.com>
2022-11-27 09:23:35 +0100

    userName is getting from server

--- src/Components/Organisms/UserAccountCard.tsx
+++ src/Components/Organisms/UserAccountCard.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from "react";
 import { View } from 'react-native';
 import { LargeLabel } from '../Atoms/LargeLabel';
 import { SmallNote } from '../Molecules/SmallNote';
 import { UserProfilePhoto } from './UserProfilePhoto';
+import { UserContext } from "../../index";
 
 export const UserAccountCard = () => {
+  const userDataProvider = useContext(UserContext);
+
   return (
     <View className={'flex h-96 w-screen flex-row items-center justify-start '}>
       <UserProfilePhoto />
       <View className={'flex-column flex'}>
-        <LargeLabel content={'User_name'} customStyles={'ml-8'} />
+        <LargeLabel content={userDataProvider ? userDataProvider?.userData?.login : ''} customStyles={'ml-8'} />
         <SmallNote
           childrenContentOn={false}
           secondPartCustomStyles={'text-orange ml-0'}
