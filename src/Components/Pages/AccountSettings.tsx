7bcfa31325146f526420e7801a33e06065997ae3
grzesikb <grzesikb@wp.pl>
2022-12-07 19:39:34 +0100

    scroll account settings

--- src/Components/Pages/AccountSettings.tsx
+++ src/Components/Pages/AccountSettings.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RootStackParamList } from '../Organisms/DrawerMenu';
@@ -19,61 +19,65 @@ export const AccountSettings = ({ navigation, route }: Props) => {
   const [userName, setUserName] = useState('');
 
   return (
-    <SafeAreaView className={'min-h-screen bg-black'}>
-      <ScreenNameWBackArrow
-        screenName={'Account settings'}
-        onPress={() => navigation.goBack()}
-      ></ScreenNameWBackArrow>
-      <View
-        className={'min-w-max flex-1 items-center justify-start bg-black pt-20'}
-      >
-        <UserProfilePhoto />
-        <AppButton
-          onPress={() => {
-            console.log('CHANGE IMAGE CLICKED');
-          }}
-          textContext={'CHANGE IMAGE'}
-        />
-        <CustomInput
-          value={userName}
-          setter={setUserName}
-          defaultValue={userDataProvider?.userData.login ?? ''}
-          customStyles={'pt-32'}
-        />
-        <AppButton
-          onPress={() => {
-            userDataProvider?.setUserData({
-              login: userName,
-              password: userDataProvider?.userData.password,
-            });
-            void (async () => {
-              const userRef = doc(db, 'user', 'GGE7ch9o6f3wxPf7JBd2');
-              await setDoc(userRef, { login: userName }, { merge: true });
-            })();
-          }}
-          textContext={'SAVE'}
-        />
-        <SmallNote firstPartText={'Reset All data'} childrenContentOn={true}>
-          <PressableText
+    <ScrollView>
+      <SafeAreaView className={'min-h-screen bg-black'}>
+        <ScreenNameWBackArrow
+          screenName={'Account settings'}
+          onPress={() => navigation.goBack()}
+        ></ScreenNameWBackArrow>
+        <View
+          className={
+            'min-w-max flex-1 items-center justify-start bg-black pt-20'
+          }
+        >
+          <UserProfilePhoto />
+          <AppButton
             onPress={() => {
+              console.log('CHANGE IMAGE CLICKED');
+            }}
+            textContext={'CHANGE IMAGE'}
+          />
+          <CustomInput
+            value={userName}
+            setter={setUserName}
+            defaultValue={userDataProvider?.userData.login ?? ''}
+            customStyles={'pt-32'}
+          />
+          <AppButton
+            onPress={() => {
+              userDataProvider?.setUserData({
+                login: userName,
+                password: userDataProvider?.userData.password,
+              });
               void (async () => {
                 const userRef = doc(db, 'user', 'GGE7ch9o6f3wxPf7JBd2');
-                const defUser = doc(db, 'user', 'pL6F37UkxvL8Rwdj7KIc');
-                const docSNap = await getDoc(defUser);
-                if (!docSNap.exists()) return;
-                const defLogin = (docSNap.data()?.login as string) ?? '';
-                await setDoc(userRef, { login: defLogin }, { merge: true });
-                userDataProvider?.setUserData({
-                  login: defLogin,
-                  password: userDataProvider?.userData.password,
-                });
+                await setDoc(userRef, { login: userName }, { merge: true });
               })();
             }}
-          >
-            RESET
-          </PressableText>
-        </SmallNote>
-      </View>
-    </SafeAreaView>
+            textContext={'SAVE'}
+          />
+          <SmallNote firstPartText={'Reset All data'} childrenContentOn={true}>
+            <PressableText
+              onPress={() => {
+                void (async () => {
+                  const userRef = doc(db, 'user', 'GGE7ch9o6f3wxPf7JBd2');
+                  const defUser = doc(db, 'user', 'pL6F37UkxvL8Rwdj7KIc');
+                  const docSNap = await getDoc(defUser);
+                  if (!docSNap.exists()) return;
+                  const defLogin = (docSNap.data()?.login as string) ?? '';
+                  await setDoc(userRef, { login: defLogin }, { merge: true });
+                  userDataProvider?.setUserData({
+                    login: defLogin,
+                    password: userDataProvider?.userData.password,
+                  });
+                })();
+              }}
+            >
+              RESET
+            </PressableText>
+          </SmallNote>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
