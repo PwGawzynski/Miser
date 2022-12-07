c567c508eda8e3cc4e4dfb05ecc01e162ff0344c
pwgawzynski <pgawzynski.backup@gmail.com>
2022-12-07 17:00:18 +0100

    Firestore updated

--- config.js
+++ config.js
@@ -1,21 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDLi8kY5pD8my-2e_ilM3vdssbK2heZ5tI',
-  authDomain: 'miser-95d10.firebaseapp.com',
-  projectId: 'miser-95d10',
-  storageBucket: 'miser-95d10.appspot.com',
-  messagingSenderId: '597990325921',
-  appId: '1:597990325921:web:34253fdfa305344e60fae0',
-  measurementId: 'G-GM601ZNYKK',
+  apiKey: "AIzaSyDAVm7oxWt3LfgGDAiknh8SgEafKqgSdF4",
+  authDomain: "misserv.firebaseapp.com",
+  projectId: "misserv",
+  storageBucket: "misserv.appspot.com",
+  messagingSenderId: "1072776762886",
+  appId: "1:1072776762886:web:0a8e9e730488e8dcd05587",
+  measurementId: "G-GX1JTTGH5L"
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+
+
+
+
 export const db = getFirestore(app);
+
+
