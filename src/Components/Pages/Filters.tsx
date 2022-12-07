5e1c2962bfd31b40254dbbff23a11e8a22018db3
pwgawzynski <pgawzynski.backup@gmail.com>
2022-12-07 18:21:07 +0100

    filters done

--- src/Components/Pages/Filters.tsx
+++ src/Components/Pages/Filters.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Text, View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../index';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { RootStackParamList } from "../Organisms/DrawerMenu";
+import { SmallLabel } from "../Atoms/SmallLabel";
+import { CustomInput } from "../Atoms/CustomInput";
+import { ScreenNameWBackArrow } from "../Molecules/ScreenNameWBackArrow";
+import { AppButton } from "../Atoms/AppButton";
 type Props = NativeStackScreenProps<RootStackParamList, 'Filters'>;
 export const Filters = ({ navigation, route }: Props) => {
+  const [hotDealValue, setHotDealValue] = useState('');
+  const [goodLowerScore, SetGoodLowerScore] = useState('');
+  const [goodHigherScore, setGoodHigherScore] = useState('');
+  const [days, setDays] = useState('');
   return (
-    <SafeAreaView className={'min-h-screen bg-midnight'}>
-      <View className={'min-w-max flex-1 items-center justify-center bg-metal'}>
-        <Text className={'text-white'}>Witam w Filters kurwa.</Text>
+    <SafeAreaView className={'bg-black w-screen h-screen flex flex-column justify-start align-center'}>
+    <View className={'h-max w-max'}>
+      <ScreenNameWBackArrow
+        screenName={'Filters'}
+        onPress={navigation.goBack}
+      />
+      <View className={'w-max m-6'}>
+        <SmallLabel customStyles={'ml-0 text-xl'} content={`Set the value of hot deal's`}/>
+        <CustomInput customStyles={'w-6/12 text-3xl mt-4'} value={hotDealValue} setter={setHotDealValue} defaultValue={'1000'}/>
       </View>
+
+      <View className={'w-max m-6'}>
+        <SmallLabel customStyles={'ml-0 text-xl'} content={`Select the point range displayed in the GOOD tab`}/>
+        <View className={'flex flex-row w-max'}>
+          <CustomInput customStyles={'w-4/12 text-3xl mt-4 mr-24'} value={goodLowerScore} setter={SetGoodLowerScore} defaultValue={'500'}/>
+          <CustomInput customStyles={'w-4/12 text-3xl mt-4'} value={goodHigherScore} setter={setGoodHigherScore} defaultValue={'800'}/>
+        </View>
+      </View>
+
+      <View className={'w-max m-6'}>
+        <SmallLabel customStyles={'ml-0 text-xl'} content={`After how many days the NEW deadline expires`}/>
+        <View className={'w-max flex flex-row'}>
+          <CustomInput customStyles={'w-2/12 text-3xl mt-4'} value={days} setter={setDays} defaultValue={'1000'}/>
+          <Text className={'color-blackGray text-3xl color-white h-max pt-6 pl-4'}>day's</Text>
+        </View>
+      </View>
+    </View>
+    <AppButton customStyles={'w-screen  m-0 p-0 mt-60'} onPress={navigation.goBack} textContext={"SAVE"}/>
     </SafeAreaView>
-  );
+);
 };
